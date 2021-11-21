fdee85a19300a0c510e39286648f9fba35f213b0
Josh Daniel <joshpersonal8@gmail.com>
2021-11-21 15:02:35 +0800

    Feat: add options

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,54 @@
 import React, { useState, useRef } from 'react';
 import Output from './components/Output';
-import { BsImageFill, BsEyeFill, BsPenFill, BsStar } from 'react-icons/bs';
+import { BsImageFill, BsEyeFill, BsStarFill } from 'react-icons/bs';
+import { MdArrowDropDown, MdArrowDropUp, MdFilter } from 'react-icons/md';
+
+export interface OutputProps {
+  file: File | null;
+  textBg: string;
+  fontSize: number;
+  lineHeight: number;
+  grayscale: boolean;
+  multiplyText: number;
+}
 
 const App: React.FC = () => {
   const [textBg, setTextBg] = useState('');
+  const [fontSize, setFontSize] = useState(16);
+  const [lineHeight, setLineHeight] = useState(10);
+  const [multiplyText, setMultiplyText] = useState(1);
+
   const [showFull, setShowFull] = useState(false);
+  const [grayscale, setGrayscale] = useState(false);
   const [file, setFile] = useState<File | null>(null);
-  const [isGenerated, setIsGenerated] = useState(false);
 
   const fileRef = useRef<HTMLInputElement>(null);
 
-  if (showFull) return <Output textBg={textBg} file={file} />;
+  const outputProps: OutputProps = {
+    file,
+    textBg,
+    fontSize,
+    lineHeight,
+    grayscale,
+    multiplyText,
+  };
 
-  const canGenerate = file && !isGenerated;
+  if (showFull) return <Output {...outputProps} />;
 
   return (
     <main className='flex mx-auto justify-center mt-8 space-x-4'>
-      <div className='p-8 bg-secondary text-gray-200 rounded-lg flex flex-col items-center w-[360px] space-y-3'>
+      <div className='p-8 bg-secondary text-gray-200 rounded-lg flex flex-col items-center w-[360px] space-y-2'>
         <a
-          className='btn bg-green-600 w-full hover:bg-green-600/90 hover:shadow-lg'
+          className='btn bg-green-600  hover:bg-green-600/90 hover:shadow-lg'
           href='https://github.com/joshxfi/text-portrait-generator'
           target='_blank'
           rel='noreferrer noopener'
         >
           <p>Star on GitHub</p>
-          <BsStar />
+          <BsStarFill />
         </a>
 
-        <div className='flex justify-between w-full'>
+        <div className='flex justify-between w-full space-x-2'>
           <button
             onClick={() => fileRef.current?.click()}
             className='btn primary-bg'
@@ -48,45 +69,106 @@ const App: React.FC = () => {
         </div>
 
         <textarea
+          spellCheck={false}
           value={textBg}
           onChange={e => setTextBg(e.target.value)}
           placeholder='Input text background'
           className='text-black resize-none w-full outline none p-4 h-[250px] outline-none rounded-lg'
         />
 
-        <button
-          onClick={() => canGenerate && setIsGenerated(true)}
-          className={`${
-            !canGenerate ? 'bg-gray-500 cursor-not-allowed' : 'primary-bg'
-          } btn w-full`}
-        >
-          <p>Generate</p>
-          <BsPenFill />
-        </button>
+        <div className='grid grid-cols-6 w-full gap-2'>
+          <button
+            onClick={() => setFontSize(fs => fs - 1)}
+            className='btn border border-red-500'
+          >
+            <MdArrowDropDown className='text-2xl' />
+          </button>
 
-        <input
-          ref={fileRef}
-          className='hidden'
-          type='file'
-          onChange={e => {
-            const file = e.target.files;
-            if (file) setFile(file[0]);
-          }}
-          accept='image/png, image/gif, image/jpeg'
-        />
+          <p className='col-span-4 btn'>Font Size [ {fontSize}px ]</p>
+
+          <button
+            onClick={() => setFontSize(fs => fs + 1)}
+            className='btn border border-green-500'
+          >
+            <MdArrowDropUp className='text-2xl' />
+          </button>
+        </div>
+
+        <div className='h-[1px] bg-gray-600 w-full' />
+
+        <div className='grid grid-cols-6 w-full gap-2'>
+          <button
+            onClick={() => setLineHeight(lh => lh - 1)}
+            className='btn border border-red-500'
+          >
+            <MdArrowDropDown className='text-2xl' />
+          </button>
+
+          <p className='col-span-4 btn'>Spacing [ {lineHeight}px ]</p>
+
+          <button
+            onClick={() => setLineHeight(lh => lh + 1)}
+            className='btn border border-green-500'
+          >
+            <MdArrowDropUp className='text-2xl' />
+          </button>
+        </div>
+
+        <div className='h-[1px] bg-gray-600 w-full' />
+
+        <div className='grid grid-cols-6 w-full gap-2'>
+          <button
+            onClick={() => setMultiplyText(mt => mt - 1)}
+            className='btn border border-red-500'
+          >
+            <MdArrowDropDown className='text-2xl' />
+          </button>
+
+          <p className='col-span-4 btn'>Mutiply Text [ {multiplyText}x ]</p>
+
+          <button
+            onClick={() => setMultiplyText(mt => mt + 1)}
+            className='btn border border-green-500'
+          >
+            <MdArrowDropUp className='text-2xl' />
+          </button>
+        </div>
+
+        <button
+          onClick={() => setGrayscale(!grayscale)}
+          className={`${
+            grayscale
+              ? 'bg-green-600 hover:bg-green-600/90'
+              : 'bg-gray-500 hover:bg-gray-500/90'
+          } btn hover:shadow-lg`}
+        >
+          <p>Grayscale</p>
+          <MdFilter />
+        </button>
       </div>
 
       <div
         className={`${
-          !isGenerated && 'p-8'
+          !file && 'p-8'
         } bg-secondary rounded-lg w-[650px] h-[650px] grid place-items-center overflow-hidden`}
       >
-        {isGenerated ? (
-          <Output textBg={textBg} file={file} />
+        {file ? (
+          <Output {...outputProps} />
         ) : (
           <p className='text-gray-200 text-2xl'>Text Portrait Generator</p>
         )}
       </div>
+
+      <input
+        ref={fileRef}
+        className='hidden'
+        type='file'
+        onChange={e => {
+          const file = e.target.files;
+          if (file) setFile(file[0]);
+        }}
+        accept='image/png, image/gif, image/jpeg'
+      />
     </main>
   );
 };
--- src/components/Output.tsx
+++ src/components/Output.tsx
@@ -1,26 +1,36 @@
 import React, { useMemo } from 'react';
+import { OutputProps } from '../App';
 
-interface OutputProps {
-  file: File | null;
-  textBg: string;
-}
-
-const Output: React.FC<OutputProps> = ({ file, textBg }) => {
+const Output: React.FC<OutputProps> = ({
+  file,
+  textBg,
+  fontSize,
+  lineHeight,
+  grayscale,
+  multiplyText,
+}) => {
   const photoUrl = useMemo(() => file && URL.createObjectURL(file), [file]);
 
   return (
     <div className='w-full h-full flex items-center justify-center bg-black overflow-hidden'>
+      {!textBg && (
+        <div className='text-gray-200'>
+          <p>Start Typing...</p>
+        </div>
+      )}
       <p
-        style={{ backgroundImage: `url(${photoUrl})` }}
-        className='leading-[10px] bg-clip-text bg-fixed bg-cover bg-no-repeat bg-center'
+        style={{
+          backgroundImage: `url(${photoUrl})`,
+          fontSize,
+          lineHeight: `${lineHeight}px`,
+        }}
+        className={`${
+          grayscale && 'grayscale'
+        } leading-[10px] bg-clip-text bg-fixed bg-cover bg-no-repeat bg-center`}
       >
-        {Array(100)
+        {Array(multiplyText)
           .fill(0)
-          .map(() =>
-            textBg
-              ? textBg
-              : 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.'
-          )}
+          .map(() => textBg)}
       </p>
     </div>
   );
